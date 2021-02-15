5d93430be14a2db423c46fac124ee4fd67992f35
wolfbotz-seguidor <59606037+wolfbotz-seguidor@users.noreply.github.com>
2021-02-15 11:09:00 -0300

    Update main.c

--- PIC 18F4550/main.c
+++ PIC 18F4550/main.c
@@ -5,22 +5,22 @@
  * Created on 2 de Fevereiro de 2021, 19:28
  */
 #include "configbit.txt" //configurações dos bits
-#include <xc.h>
+#include <xc.h>          //compilador da IDE
 #include <pic18F4550.h>
 #include <stdio.h>
 #define _XTAL_FREQ 20000000 //Frequencia em 20MHz
 
-void aula01(){
+/*void aula01(){
     //código que acende e apagua o led
     
     TRISB = 0b00000000; //declara todas as portas do PORTB como saída
     PORTB = 0b00000000; //Inicializa todas em 0
     
     while(1){
-        RB1 = 1; //Porta RB1
+        RB1 = 1; //Porta RB1 ligada
         __delay_ms(100);
         
-        RB1 = 0;
+        RB1 = 0;  //Porta RB1 desligada
         __delay_ms(100);
     }
 }
@@ -29,11 +29,11 @@ void aula02(){
     //código que vai acendendo os leds em sequência de bits
     
 //         RB 76543210
-    TRISB = 0b00000000;
+    TRISB = 0b00000000;  //sequência binária que indica qual porta é do registrador do RB0 até do RB7
     PORTB = 0b00000000;
     
     while(1){
-        PORTB++; //incrementando no PORTB
+        PORTB++; //incrementando no PORTB acendendo uma porta por vez
         __delay_ms(500);
         
     }
@@ -69,7 +69,7 @@ void aula04(){ //Esse código
     
     
     while(1){
-        if(RB0 == 1){
+        if(RB0 == 1){  //quando o botão está pessrionado, borão em pull up, se não em engano
             RB1 = 1;
             __delay_ms(2000);
         }
@@ -102,22 +102,20 @@ void aula05(){
         
     }
 
-}
+}*/
 
 
-#define botao1 RC0
+/*#define botao1 RC0
 #define botao2 RC1
 #define D1 RD0
-#define D2 RD1 //define a porta com um nome, LED no caso*/
+#define D2 RD1 //define a porta com um nome, LED no caso
 
 void aula06(){
     //código que acende e apaga o led através de um botão
     
-    CMCON = 7; //Serve para alterar a função da porta entre A e D 7=111 em binário
-    // nessa configuração RA0, RA1, RA2 e RA3 como portas digitais.
-    
+    CMCON = 7; //Desabilita os comparadores das portas
     TRISC = 0b00000011; //RC0 e RC1 como entradas digitais
-    TRISD = 0b00000000;
+    TRISD = 0b00000000;  //todas as portas como saídas
     
     botao1 = 1;
     botao2 = 1;
@@ -138,13 +136,13 @@ void aula06(){
     }
 
 
-}
+}*/
 
 
-void aula07(){
+/*void aula07(){
     //código que acende os leds em cadeia com shift left
     
-    int control = 0xFF;
+    int control = 0xFF;  //variável definida em hexadecimal, todos os bit em estado alto.
     
     TRISD = 0; //Todos os terminais estão como saída
     PORTD = 0x00; //Inicia todos em 0, notação em hexadecimal
@@ -155,6 +153,7 @@ void aula07(){
         
         control = control << 1;  //shift left = deslocamento de bit para esquerda.
                                 //Do menos significativo para o mais significativo
+                               //deslocando sempre o bit um para a esquerda
         
         if(control == 0){
             control = 0xFF;
@@ -163,9 +162,9 @@ void aula07(){
     }
     
     
-}
+}*/
 
-#define botao1 RC0 
+/*#define botao1 RC0 
 #define botao2 RC1
 #define led1 RD6
 #define led2 RD7
@@ -174,7 +173,7 @@ void aula08(){
     CMCON = 7;
     TRISC = 0x03;
     TRISD = 0x00;
-    PORTC = 0x03;
+    PORTC = 0x03; //define as últimas portas como entradas
     
     while(1){
         if(botao1 == 0){
@@ -202,16 +201,16 @@ void aula08(){
         }
     }
     
-}
+}*/
 
-int numbers;
+/*int numbers;
 
 void aula09(){
     CMCON = 7; //desbilita os comparadores
     TRISD = 0x00; 
     TRISC = 0x03; //indica os bits menos significativos como entrada
     
-    PORTC = 0x03;
+    PORTC = 0x03;  //inicializa as últimas portas com nível lógico alto.
     PORTD = 0x00;
     
     while(1){
@@ -244,10 +243,10 @@ void aula09(){
     }
     
 
-}
+}*/
 
 
-#define botao1 RC0 
+/*#define botao1 RC0 
 #define botao2 RC1
 #define led1 RD6
 #define led2 RD7
@@ -306,7 +305,7 @@ void servo_centro(){
     __delay_us(18500);
     servo = 0x01;
     __delay_us(1500); //pulso de 1,5ms
-}
+}*/
 
 
 
@@ -332,7 +331,7 @@ TACQ = 4 * TDA = 3.2us
 
 tempo de conv = 11*TDA + TACQ = 12us*/
 
-#define led1 RD7
+/*#define led1 RD7
 #define led2 RD6
 #define led3 RD5
 #define led4 RD4
@@ -366,7 +365,7 @@ void aula11(){ // esse código está funcionando parcialmente
         ADCON0 = 0b00000101;// Seleciona canal AN1
         ADCON0bits.GO=1; // inicia a conversão
         while (ADCON0bits.GO); // Aguarda a o fim da conversão
-        leitura2 = ADRESL + (ADRESH << 8);
+        leitura2 = ADRESL + (ADRESH << 8);  //agrega a variável os 10 bits do conversor AD
         
         
         //int leitura_total[] = {leitura1, leitura2};
@@ -433,10 +432,10 @@ void aula11(){ // esse código está funcionando parcialmente
             led8 = 1;
         }
     }
-        __delay_ms(500); //taxa de atualização do ADC*/
-}
+        __delay_ms(500); //taxa de atualização do ADC
+}*/
         
-void aula12(){ //código para exibir no display de 7 segmentos
+/*void aula12(){ //código para exibir no display de 7 segmentos
     unsigned char catodo, cont = 0x00;
     unsigned char segmento [] ={0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x67}; //determina o que será mostrado no display convertido em hexadecimal
                                 //valores de 0 a 9 em hexadecimal.
@@ -457,10 +456,10 @@ void aula12(){ //código para exibir no display de 7 segmentos
     
     
     }
-}
+}*/
 
 
-void aula13(){
+/*void aula13(){
     //este programa faz a contagem de dois displays de 7 segmentos de 1 a 99 usando o CI 4511
     unsigned char catodo1, catodo2, cont1, cont2 = 0x00;
     unsigned char segmento1 [] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
@@ -494,10 +493,10 @@ void aula13(){
 
 
 
-}
+}*/
 
 
-#define led1 RD7
+/*#define led1 RD7
 #define led2 RD6
 #define botao1 RC0
 #define botao2 RC1
@@ -521,11 +520,139 @@ void aula14(){
     
     }
 
-}
+}*/
+
+
+/*#define DIGIT1 RB1
+#define DIGIT2 RB2
+#define DIGIT3 RB3
+#define MAIS RA0
+#define MENOS RA1
+
+int Cen, Msd, Lsd, Cnt =0;
+int Display (int no);
+void changeCount();
 
 void aula15(){
+    CMCON = 0x07;
+    TRISB = 0x00; //saída
+    TRISA = 0x03 //entrada
+    DIGIT2 = 0; //desabilita dígito2
+    DIGIT3 = 0; //desabilita dígito3
+    
+    while(1){
+        changeCount();
+        Cen = (Cnt/100)-((Cnt%100)/100); //Dígito das centenas
+        
+        PORTB = Display(Cen); //Envia para o PORTB
+        DIGIT1 = 1;           //habilita dígito1
+        __delay_ms(5);
+        DIGIT1 = 0;
+        
+        Msd = (Cnt%100);
+        Msd = (Msd/10) - ((Msd%10)/10);
+        
+        PORTB = Display(Msd);
+        DIGIT2 = 1;  //habilita dígito 2(dezena)
+        __delya_ms(5);
+        DIGIT2 = 0;
+        
+        
+        Lsd = Cnt%10;
+        
+        PORTB = Display(Lsd);
+        DIGIT3 = 1;
+        __delay_ms(5);
+        DIGIT3 = 0;
+    
+    }
+    
+}
 
 
+//Essa função acha o bit pattern(matrix de bits) para ser enviado
+//para o PORTB e mostrar o número no display de 7 segmentos. O número
+//é passado como um argumento para a função
+
+int Display(int no){
+    int Pattern;
+    int SEGMENTO[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x67};
+    Pattern = SEGMENTO [no]; //para retornar o PAttern
+    return (Pattern);
+
+
+}
+
+void changeCount(){
+    while(MAIS == 1){
+        Cnt += 10;
+        __delay_ms(180);
+        if(Cnt > 300) Cnt = 300;
+    
+    }
+    
+    while(MENOS == 1){
+        Cnt -= 10;
+        __delay_ms(180);
+        if(Cnt < 1) Cnt = 0;
+    
+    }
+    
+
+
+}*/
+
+int counter = 0x00;
+
+void __interrupt() isr(void)      //vetor de interrupção
+{
+    if(INTCONbits.TMR0IF){          //Testando se houve estouro no Timer0
+        counter++;                  //Incrementa o counter
+        TMR0 = 0x00;                //Reinicia o registrador Timer0
+        INTCONbits.TMR0IF = 0x00;   //limpa a flag para a próxima interrupção
+        
+        /*if(counter == 2500){  //22ms /100 = 0,22ms = 220us
+            RB4 = ~RB4;
+            counter = 0;
+        }*/
+    
+    }
+
+}
+
+
+void aula16(){
+    //-------------Região dos registradores do TIMER0---------------//
+    
+    T0CONbits.T08BIT = 0x01;           //configura o TMR0 como contador de 8 bits
+    T0CONbits.T0CS = 0x00;              //clock externo
+    T0CONbits.PSA = 0x00;              //associa os pre scaler ao módulo timer0
+    T0CONbits.T0PS2 = 0x00;
+    T0CONbits.T0PS1 = 0x00;
+    T0CONbits.T0PS0 = 0x01;            //Define o valor do prescaler no caso em 1:4 --> 1 no T0PS0 associado ao tmr0
+    INTCON2bits.RBPU = 0x01;           //desabilita os resistors pull-ups internos*/
+
+    //---------------------------------------------------------------//
+    
+    INTCONbits.GIE = 0x01;             //Habilita a interrupção global
+    INTCONbits.PEIE = 0x01;            //Habilita a interrupção por periféricos
+    INTCONbits.TMR0IE = 0x01;          //Habilita ainterrupção por estouro do Timer0
+    
+    TMR0 = 0x12;                        //inicia a contagem em 12
+
+    TRISB = 0x00;               //configura a porta RB4 como saída digital
+    PORTB = 0x00;               //Inicilaiza RB4 em LOW
+
+    while(1){
+        
+        if(counter == 2560){            //ciclo de máquina = 1/(Freq. /4 ) = 0,2057us
+                                         //a cada 0,2057us * 4 * 244 = 0,2007ms --tempo para incrementar no counter
+                                        //0,2007 * 4 * 244 * 2560 = 500ms
+            RB4 = ~RB4;
+            counter = 0x00;
+        }
+        
+    }
 }
 
 void main(void) {
@@ -543,11 +670,11 @@ void main(void) {
     //aula12();
     //aula13();
     //aula14();
-    aula15();
+    //aula15();
+    aula16();
     
     
     
     
 
 }
-
