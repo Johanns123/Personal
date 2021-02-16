e30d9c690fe58616d397da11b4add31dd66e9bb7
wolfbotz-seguidor <59606037+wolfbotz-seguidor@users.noreply.github.com>
2021-02-16 16:00:32 -0300

    Update pwm.h

--- PWM.X/pwm.h
+++ PWM.X/pwm.h
@@ -1,23 +1,7 @@
-/* ########################################################################
-   PICsim - PIC simulator http://sourceforge.net/projects/picsim/
-   ########################################################################
-   Copyright (c) : 2015  Luis Claudio Gambôa Lopes
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-   For e-mail suggestions :  lcgamboa@yahoo.com
-   ######################################################################## */
+//duty1 -> motor B, da esquerda
+//duty2 -> motor A, da direita
 
-  #define _XTAL_FREQ 20000000
-
-  void PWM1_Init(unsigned int);
-  void PWM1_Start(void);
-  void PWM1_Set_Duty(unsigned char); 
+//Protótipos:
+void PWM_Init ();
+void PWM1_Set_Duty (unsigned short duty1);
+void PWM2_Set_Duty (unsigned short duty2);
