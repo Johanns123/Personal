ce40e75f512489387c9f3cbb338f1facca9e8fd6
wolfbotz-seguidor <59606037+wolfbotz-seguidor@users.noreply.github.com>
2021-02-16 16:00:26 -0300

    Update pwm.c

--- PWM.X/pwm.c
+++ PWM.X/pwm.c
@@ -1,103 +1,45 @@
-/* ########################################################################
-   PICsim - PIC simulator http://sourceforge.net/projects/picsim/
-   ########################################################################
-   Copyright (c) : 2015  Luis Claudio Gambôa Lopes
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-   For e-mail suggestions :  lcgamboa@yahoo.com
-   ######################################################################## */
-
+//#include "configbit.txt" //configurações dos bits
 #include <xc.h>
 
-#include"pwm.h"
+#include "PWM.h"
 
+//Funções:
 
- void PWM1_Init(unsigned int f)
-  {
-   unsigned int temp;
- //PWM Period = [(PR2) + 1] * 4 * TOSC *(TMR2 Prescale Value)
- //PWM Duty Cycle = (CCPRXL:CCPXCON<5:4>) *TOSC * (TMR2 Prescale Value)
-
-
-   //desliga PWM
-     CCP1CON=0x00;//CCP disabled
-     TRISCbits.TRISC2=1; //desliga saídas PWM
-     TRISDbits.TRISD5=1;
-
-     PORTCbits.RC2=0; //deliga saídas PWM
-     PORTDbits.RD5=0;
-
-      CCPR1L=0;//ou 255?
-
-
-     //calculo TMR2
-
-      T2CONbits.TMR2ON=0;
-             
-     temp=_XTAL_FREQ/(f*4l);
+void PWM_Init () {
+    //periodo do pwm = (PR2 + 1) * ciclo de máquina * prescaler do timer2
+    //periodo = (240+1) * 0,2us * 16 = 0,8192ms
+    //frequencia = 1/periodo = 1220,70Hz = 1,29kHz
     
-     if (temp < 256)
-     {
-       T2CONbits.T2CKPS=0;  //1
-       PR2=temp;
-     }
-     else if(temp/4 < 256 )
-     {
-       T2CONbits.T2CKPS=1;  //4
-       PR2=(temp+2)/4;
-     }
-     else
-     {
-       PR2=(temp+8)/16;
-       T2CONbits.T2CKPS=2;  //16
-     }
+    PR2 = 240;
 
-     T2CONbits.TOUTPS=0;  //1-16
-     
-  }
+    T2CKPS1 = 1; 
+    T2CKPS0 = 1;   // Timer 2 Prescaler = 16 
+    TMR2ON = 1; 
+    
+    CCP1M3 = 1;
+    CCP1M2 = 1;     // Configura o pino CCP1 para o modo PWM
+    
+    CCP2M3 = 1;
+    CCP2M2 = 1;     // Configura o pino CCP2 para o modo PWM
+    
+}
 
+void PWM1_Set_Duty (unsigned short duty1)  // duty_ratio vai de 0 a 962
+{
+    
+    CCPR1L = duty1 >> 2;
+    DC1B1 = duty1 & 2;
+    DC1B0 = duty1 & 1; 
+    
+}
 
-  void PWM1_Start(void)
-  {
+void PWM2_Set_Duty (unsigned short duty2)  // duty_ratio vai de 0 a 962
+{
+    
+    CCPR2L = duty2 >> 2;
+    DC2B1 = duty2 & 2;
+    DC2B0 = duty2 & 1; 
+    
+}
 
-      TRISCbits.TRISC2=0; //liga saídas PWM
-      TRISDbits.TRISD5=0;
-
-
-      CCP1CON=0x0F; //CCP -> PWM mode 0x0F
-
-      
-      T2CONbits.TMR2ON=1;
- 
-      //espera PWM normalizar
-
-      PIR1bits.TMR2IF=0;
-      while(PIR1bits.TMR2IF == 0);
-      PIR1bits.TMR2IF=0;
-      while(PIR1bits.TMR2IF == 0);
-
-     
-
-
-  }
-
-
-  void PWM1_Set_Duty(unsigned char d)
-  {
-      unsigned int temp;
-      
-      temp=(((unsigned long)(d))*((PR2<<2)|0x03))/255;
-
-      CCPR1L= (0x03FC&temp)>>2;
-      CCP1CON=((0x0003&temp)<<4)|0x0F;
-  }
 
