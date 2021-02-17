0007eeb8691920d9d23eaa7431a7cd51f0db1614
wolfbotz-seguidor <59606037+wolfbotz-seguidor@users.noreply.github.com>
2021-02-16 22:11:01 -0300

    Create PWM.c

---
+++ Códido base em PIC 18f4550.X/PWM.c
@@ -0,0 +1,44 @@
+#include <xc.h>
+
+#include "PWM.h"
+
+//Funções:
+
+void PWM_Init () {
+    //periodo do pwm = (PR2 + 1) * ciclo de máquina * prescaler do timer2
+    //periodo = (240+1) * 0,2us * 16 = 0,8192ms
+    //frequencia = 1/periodo = 1220,70Hz = 1,29kHz
+    
+    PR2 = 240;
+
+    T2CONbits.T2CKPS1 = 1; 
+    T2CONbits.T2CKPS0 = 1;   // Timer 2 Prescaler = 16 
+    T2CONbits.TMR2ON = 1; 
+    
+    CCP1CONbits.CCP1M3 = 1;
+    CCP1CONbits.CCP1M2 = 1;     // Configura o pino CCP1 para o modo PWM
+    
+    CCP2CONbits.CCP2M3 = 1;
+    CCP2CONbits.CCP2M2 = 1;     // Configura o pino CCP2 para o modo PWM
+    
+}
+
+void PWM1_Set_Duty (unsigned short duty1)  // duty_ratio vai de 0 a 962
+{
+    
+    CCPR1L = duty1 >> 2;
+    CCP1CONbits.DC1B1 = duty1 & 2;
+    CCP1CONbits.DC1B0 = duty1 & 1; 
+    
+}
+
+void PWM2_Set_Duty (unsigned short duty2)  // duty_ratio vai de 0 a 962
+{
+    
+    CCPR2L = duty2 >> 2;
+    CCP2CONbits.DC2B1 = duty2 & 2;
+    CCP2CONbits.DC2B0 = duty2 & 1; 
+    
+}
+
+
