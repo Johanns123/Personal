b46d0a0aba622a18469ba9ce2ecc72cf5ca70f35
wolfbotz-seguidor <59606037+wolfbotz-seguidor@users.noreply.github.com>
2021-02-16 21:06:11 -0300

    Update pwm.c

--- PWM.X/pwm.c
+++ PWM.X/pwm.c
@@ -1,4 +1,3 @@
-//#include "configbit.txt" //configurações dos bits
 #include <xc.h>
 
 #include "PWM.h"
@@ -12,15 +11,15 @@ void PWM_Init () {
     
     PR2 = 240;
 
-    T2CKPS1 = 1; 
-    T2CKPS0 = 1;   // Timer 2 Prescaler = 16 
-    TMR2ON = 1; 
+    T2CONbits.T2CKPS1 = 1; 
+    T2CONbits.T2CKPS0 = 1;   // Timer 2 Prescaler = 16 
+    T2CONbits.TMR2ON = 1; 
     
-    CCP1M3 = 1;
-    CCP1M2 = 1;     // Configura o pino CCP1 para o modo PWM
+    CCP1CONbits.CCP1M3 = 1;
+    CCP1CONbits.CCP1M2 = 1;     // Configura o pino CCP1 para o modo PWM
     
-    CCP2M3 = 1;
-    CCP2M2 = 1;     // Configura o pino CCP2 para o modo PWM
+    CCP2CONbits.CCP2M3 = 1;
+    CCP2CONbits.CCP2M2 = 1;     // Configura o pino CCP2 para o modo PWM
     
 }
 
@@ -28,8 +27,8 @@ void PWM1_Set_Duty (unsigned short duty1)  // duty_ratio vai de 0 a 962
 {
     
     CCPR1L = duty1 >> 2;
-    DC1B1 = duty1 & 2;
-    DC1B0 = duty1 & 1; 
+    CCP1CONbits.DC1B1 = duty1 & 2;
+    CCP1CONbits.DC1B0 = duty1 & 1; 
     
 }
 
@@ -37,8 +36,8 @@ void PWM2_Set_Duty (unsigned short duty2)  // duty_ratio vai de 0 a 962
 {
     
     CCPR2L = duty2 >> 2;
-    DC2B1 = duty2 & 2;
-    DC2B0 = duty2 & 1; 
+    CCP2CONbits.DC2B1 = duty2 & 2;
+    CCP2CONbits.DC2B0 = duty2 & 1; 
     
 }
 
