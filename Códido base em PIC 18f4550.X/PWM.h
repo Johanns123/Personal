70e01cae766e33662f29bf7f96ef5f9a380e0a9c
wolfbotz-seguidor <59606037+wolfbotz-seguidor@users.noreply.github.com>
2021-02-16 22:11:08 -0300

    Create PWM.h

---
+++ Códido base em PIC 18f4550.X/PWM.h
@@ -0,0 +1,7 @@
+//duty1 -> motor B, da esquerda
+//duty2 -> motor A, da direita
+
+//Protótipos:
+void PWM_Init ();
+void PWM1_Set_Duty (unsigned short duty1);
+void PWM2_Set_Duty (unsigned short duty2);
