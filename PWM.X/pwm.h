7666f47a6edf23f91d9bfe8b2db6fc586f36f1e9
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2021-06-14 12:40:40 -0300

    Update pwm.h

--- PWM.X/pwm.h
+++ PWM.X/pwm.h
@@ -3,5 +3,5 @@
 
 //Protótipos:
 void PWM_Init ();
-void PWM1_Set_Duty (unsigned short pwm1);
-void PWM2_Set_Duty (unsigned short pwm2);
+void PWM1_Set_Duty (unsigned short duty1);
+void PWM2_Set_Duty (unsigned short duty2);
