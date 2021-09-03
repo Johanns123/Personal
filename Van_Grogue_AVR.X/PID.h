e0ad5e1a8779df450cc6c19b5ec05e5d8f7eb38b
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2021-09-03 15:24:57 -0300

    Update PID.h

--- Van_Grogue_AVR.X/PID.h
+++ Van_Grogue_AVR.X/PID.h
@@ -1,7 +1,7 @@
 int PID(int error)/*Algoritmo de controle PID usando os sensores frontais*/
 {
     static unsigned int Kp = 2, Kd = 0, Ki = 0;
-    static unsigned int prescale = 2048; //prescale na pot?ncia de 2: 2^n
+    static unsigned int prescale = 16; //prescale na potência de 2: 2^n
     static int integral = 0, erroAnterior = 0;
     int p = 0, i = 0, d = 0, Turn = 0;
    
