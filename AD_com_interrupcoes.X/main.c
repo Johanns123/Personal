9363e1f5fa9b5429b1b017b1b4bdab17406c53a9
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2021-09-20 13:39:19 -0300

    Update main.c
    Interrupção do AD e calibração

--- AD_com_interrupcoes.X/main.c
+++ AD_com_interrupcoes.X/main.c
@@ -1,47 +1,50 @@
-#define F_CPU 16000000 //define a frequência do microcontrolador - 16MHz
+/*Bibliotecas e frequência do uc*/
+#define F_CPU 16000000        //define a frequencia do uC para 16MHz
+#include <avr/io.h>           //Biblioteca geral dos AVR
+#include <avr/interrupt.h>    //Biblioteca de interrupção
+#include <stdio.h>            //Bilioteca do C
+#include <util/delay.h>       //Biblioteca geradora de atraso
 #include <avr/io.h>
-#include <util/delay.h>
-#include <avr/pgmspace.h> //para o uso do PROGMEM, gravação de dados na flash
-#include <avr/interrupt.h>//para o uso de interrupções
+#include "ADC.h"
 #include "UART.h"
-#include <stdio.h>
 #include <stdlib.h>
-#define set_bit(y,bit) (y|=(1<<bit))
-#define clr_bit(y,bit) (y&=~(1<<bit))
-#define cpl_bit(y,bit) (y^=(1<<bit))
-#define tst_bit(y,bit) (y&(1<<bit))
+
+//variáveis de comando para os registradores
+#define set_bit(y,bit) (y|=(1<<bit)) //coloca em 1 o bit x da variável Y
+#define clr_bit(y,bit) (y&=~(1<<bit)) //coloca em 0 o bit x da variável Y
+#define cpl_bit(y,bit) (y^=(1<<bit)) //troca o estado lógico do bit x da variável Y
+#define tst_bit(y,bit) (y&(1<<bit)) //retorna 0 ou 1 conforme leitura do bit
+//==============================================================
+
+/*Mapeamento de Hardware*/
+
+/*Estruturas*/
 
 
 //Variáveis globais da calibração de sensores
 unsigned int valor_max[6] = {0, 0, 0, 0, 0, 0};
-unsigned int valor_min[6] = {1023, 1023, 1023, 1023, 1023, 1023};
-unsigned int valor_max_abs = 0;
-unsigned int valor_min_abs = 1023;
+unsigned int valor_min[6] = {255, 255, 255, 255, 255, 255};
+unsigned int valor_max_abs = 255;
+unsigned int valor_min_abs = 0;
+//Variáveis globais
+//================
+int ADC_dados[6];
 
-char buffer[5]; //String que armazena valores de entrada para serem printadas
 volatile char ch; //armazena o caractere lido
-volatile char flag_com = 0; //flag que indica se houve recepção de dado
+char buffer[5]; //String que armazena valores de entrada para serem printadas
 
-void setup (void);
-void setup_logica();
-void loop (void);
-void INT_init (void);
-void ADC_init (void);
-void ADC_maq (void);
-void ADC_conv_ch (unsigned char canal);
-unsigned char ADC_ler (void);
+//Protótipo das funções
+//================
+void setup_hardware(void);
+void setup(void);
+void INT_init(void);
+void ADC_maq();
+void loop();
 void calibra_sensores();
 void seta_calibracao();
 void sensores();
 
-unsigned char ADC_dados[6]; 
 
-ISR(ADC_vect) 
-{   
-    ADC_maq();
-}
-
-/*Interrupções*/
 ISR(USART_RX_vect) 
 {
     ch = UDR0; //Faz a leitura do buffer da serial
@@ -52,103 +55,158 @@ ISR(USART_RX_vect)
 
 ISR(TIMER0_OVF_vect)
 {   
-    TCNT0 = 255;
+    TCNT0 = 240;
     static int counter = 0;
     counter++;
-    if(counter == 2)
-    {   
+    if(counter == 4)
+    {
         sensores();
         for(int i = 0; i < 6; i++)
         {
             //sprintf(buffer, "%4d", ADC_dados[i]);
-            //UART_enviaString(buffer);
             UART_enviaHex(ADC_dados[i]);
+            //UART_enviaString(buffer);
             UART_enviaCaractere('\t');
         }
-        UART_enviaCaractere('\n');
+        UART_enviaCaractere('\n'); 
         counter = 0;
-        
     }
-}
-//----------------------------------------------------------------------------------
 
-int main (void) {
+}
+
+ISR(PCINT1_vect)
+{
+
+
+
+}
+
+ISR(ADC_vect)
+{
+    ADC_maq();
+}
+
+
+int main() {
+    
+    setup_hardware();
+    ADC_init();
+    //inic_LCD_4bits();
+    sei();
     setup();
-    while(1) loop();
+    INT_init();
+
+    while (1) {
+        loop();
+
+    }
     return 0;
 }
-
-void setup (void) {
-
-    MCUCR &= 0xef;      //habilita pull up quando configurado e desabilita algumas configurações prévias do MCU
+void setup_hardware(void)
+{
+    MCUCR &= 0xef;  //Pull up interno habilitado
+    DDRB = 0xf0;    //Todo PORTB como saída
+    PORTB = 0x00;   //inicia todos do PORTD em LOW
+    DDRC = 0x00;    //PC0-PC3 como entrada
+    PORTC = 0xff;   //entradas com pull up
+    //DIDR0 = 0xff;   //desabilita entrada digital de PC1
     
-    ADC_init();
-    UART_config(16); //Inicializa a comunicação UART com 57.6kbps
-    
-    sei();
-    //----> Calibração dos Sensores frontais <----//
+}
+
+void setup(void)
+{
+    UART_config(16);
     ADC_maq();
-    calibra_sensores(); //calibração dos sensores //A calibração vai conseguir acompanhar o AD
-                                                  //ou pode ser que o vetor não seja preenchido a tempo?
-                                                  //É necessário colocar um contador
-                                                  //para depois chamar a função de calibração?
-    
-    seta_calibracao(); //estabelece o limiar dos sensores através dos valores da função de cima
-    sensores(); //determina o limiar dos sensores e printa seus valores na tela
-    
+    calibra_sensores();
+    seta_calibracao();
+    set_bit(PORTB, PB5);
     _delay_ms(500);
+    clr_bit(PORTB, PB5);
+    _delay_ms(500);
+    set_bit(PORTB, PB5);
+    _delay_ms(500);
+    clr_bit(PORTB, PB5);
+    _delay_ms(500);
+    set_bit(PORTB, PB5);
+    _delay_ms(500);
+    clr_bit(PORTB, PB5);
+    _delay_ms(500);
+    set_bit(PORTB, PB5);
+    _delay_ms(2000);
     
-    INT_init();
+    
 }
 
-void loop (void) {
-}
-
-void INT_init (void) {
+void loop()
+{
     
+
+}
+void INT_init(void)
+{
     TCCR0B = 0b00000101; //TC0 com prescaler de 1024
-    TCNT0 = 255; //Inicia a contagem em 100 para, no final, gerar 1ms
+    TCNT0 = 240; //Inicia a contagem em 56 para, no final, gerar 1ms
     TIMSK0 = 0b00000001; //habilita a interrupção do TC0
+    
+    
 }
 
-void ADC_init (void) {
-     //Fosc = 16MHz -> Tosc = 62,5ns
-    //Fadc_max = 200kHz -> Tadc_min = 5us
-    // Tadc_min / Tosc = 80 -> CK/128 - TADC_resultante = 8us
-    //Primeira conversão = 25 ciclos de AD = 25 * 8us
-    //Demais conversões = 13 ciclos de AD = 13 *8us
-    //Tempo total para todas as conversões = (25*8) + (13*8*6) = 720us
-    //720us sem contar os ciclos de máquina para executar as funções e o switch case
-    //Logo, atentar à temporização de 1ms
-    
-    
-    
-    //FADC = 1MHz
-    //Tadc = 1/1MHz = 1us
-    //Primeira Conversão = 25*1 = 25us
-    //Demais conversões = 14*1 = 14us
-    //Tempo total do prieiro ciclo = (25*1) + (14*1*5) = 95us
-    //Tempo das demais conversões = 14*1*6 = 84us
-    //Utilizando teoria de amostragem -> 168->190us
-    
-    
-    //FADC = 500kHz
-    //Tadc = 1/500kHz = 2us
-    //Primeira Conversão = 25*2 = 50us
-    //Demais conversões = 14*2 = 28us
-    //Tempo total do prieiro ciclo = (25*2) + (14*2*5) = 190us
-    //Tempo das demais conversões = 14*2*6 = 168us
-    //Utilizando teoria de amostragem -> 336->380us
-    
-    ADMUX = 0x40; //0100-0000
-    ADCSRA = 0x8c; //1000-1100
-    ADCSRB = 0x00; // 0000-0000
-    DIDR0 = 0x3f;// 0011-1111
-     
+
+
+void calibra_sensores() {
+    //=====Função que inicializa a calibração====//
+    for (int i = 0; i < 120; i++) {
+        for (int i = 0; i < 6; i++) {
+            if (ADC_dados[i] < valor_min [i]) {
+                valor_min[i] = ADC_dados[i];
+            }
+            if (ADC_dados[i] > valor_max [i]) {
+                valor_max[i] = ADC_dados[i];
+            }
+
+        }
+        
+        
+        _delay_ms(20);  //tempo o suficiente para o pessoa calibrar os sensores mecanicamente
+        
+    }
+
 }
 
-void ADC_maq (void) {
-    
+void seta_calibracao() {
+    //----> Calibração dos Sensores frontais <----//
+
+    //função que seta o limiar dos sensores
+    for (int i = 0; i < 6; i++) {
+        if (valor_min [i] > valor_min_abs && valor_min[i] !=0 ) {
+            valor_min_abs = valor_min [i];
+        } 
+        
+        if (valor_max [i] < valor_max_abs) {
+            valor_max_abs = valor_max [i];
+        }
+
+    }
+            
+}
+
+void sensores() {
+
+    //======Estabelece o limiar da leitura dos sensores====//
+    //função de correção da calibração
+    for (int i = 0; i < 6; i++) {
+        if (ADC_dados[i] < valor_min_abs) {
+            ADC_dados[i] = valor_min_abs;
+        }
+        if (ADC_dados[i] > valor_max_abs) {
+            ADC_dados[i] = valor_max_abs;
+        }
+
+    }
+}
+
+void ADC_maq () 
+{
     static unsigned char estado = 10;
     
     switch (estado) {
@@ -184,7 +242,7 @@ void ADC_maq (void) {
             break;
             
         case 5:
-            estado = 6;
+            estado = 0;
             ADC_dados[5] = ADC_ler();
             ADC_conv_ch(0);
             break;
@@ -196,76 +254,5 @@ void ADC_maq (void) {
             break; 
     }
     
-}
-
-void ADC_conv_ch (unsigned char canal) {
     
-    ADMUX &= 0xf0;
-    ADMUX |= (canal & 0x0f);
-    
-    ADCSRA |= 0x40;
-            
 }
-
-unsigned char ADC_ler ( void ) {
-    
-    /*int dado = ADCL;
-    dado |= ADCH << 8;
-    return dado;*/
-    return ADCL;
-}
-
-
-
-void calibra_sensores() {
-    //=====Função que inicializa a calibração====//
-    for (int i = 0; i < 120; i++) {
-        for (int i = 0; i < 6; i++) {
-            if (ADC_dados[i] < valor_min [i]) {
-                valor_min[i] = ADC_dados[i];
-            }
-            if (ADC_dados[i] > valor_max [i]) {
-                valor_max[i] = ADC_dados[i];
-            }
-
-        }
-        
-        
-        _delay_ms(10);  //tempo o suficiente para o pessoa calibrar os sensores mecanicamente
-        
-    }
-
-}
-
-void seta_calibracao() {
-    //----> Calibração dos Sensores frontais <----//
-
-    //função que seta o limiar dos sensores
-    for (int i = 0; i < 6; i++) {
-        if (valor_min [i] < valor_min_abs && valor_min[i] !=0 ) {
-            valor_min_abs = valor_min [i];
-        } 
-        
-        if (valor_max [i] > valor_max_abs) {
-            valor_max_abs = valor_max [i];
-        }
-
-    }
-            
-}
-
-void sensores() {
-
-    //======Estabelece o limiar da leitura dos sensores====//
-    //função de correção da calibração
-    for (int i = 0; i < 6; i++) {
-        if (ADC_dados[i] < valor_min_abs) {
-            ADC_dados[i] = valor_min_abs;
-        }
-        if (ADC_dados[i] > valor_max_abs) {
-            ADC_dados[i] = valor_max_abs;
-        }
-
-    }
-}
-
