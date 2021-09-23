faeab4007f134e27f78f26daff73c28104863bb9
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2021-09-22 21:03:51 -0300

    Update main.c
    Botão de apagar funcionando pela metade.

--- Teclado_Matricial.X/main.c
+++ Teclado_Matricial.X/main.c
@@ -1,483 +1,3 @@
-/*
- * File:   main.c
- * Author: johannsamorim
- *
- * Created on 20 de Setembro de 2021, 18:38
- */
-/*
-#include <xc.h>
-#define _XTAL_FREQ 4000000
-#include "configbits.txt"
-#include <string.h>
-#include "lcd.h"
-#include "pwm.h"
-
-
-//Mapeamento de Hardware
-#define linha1  LATB3
-#define linha2  LATB4
-#define linha3  LATB5
-#define linha4  LATA5
-
-#define disp1 LATA2
-#define disp2 LATA3
-#define disp3 LATA4
-#define disp4 LATA5
-#define display LATD
-
-
-//Variáveis globais
-//================
-char linha01[17]="LCD linha1      ", linha02[17]="LCD linha2      ";
-
-unsigned int fan = 0;
-unsigned int max_timer0 = 200, max_timer1 = 200, max_timer2 = 200;
-int cont1 = 0, cont2 = 0, cont3 = 0, cont4 = 0, max_count1 = 0, max_count2 = 0;
-unsigned char controle = 0x01;
-unsigned char numero = 0x00;
-unsigned char n1=1, n2=2, n3, n4;
-unsigned char fLCDnew=0;
-int valorADC;
-unsigned char fADCnew=0;
-unsigned char contador, pausa, reverso;
-
-
-//Protótipo das funções
-//================
-void setup_hard(void);
-void setup (void);
-void loop (void);
-void f_int0 (void);
-void f_int1 (void);
-void f_int2 (void);
-void maqdisplay (void);
-void keybdisplay(void);
-void f_timers(void);
-void f_timer0 (void);
-void f_timer1 (void);
-void f_timer2 (void);
-void fint_ADC(void);
-void mostraLCD (void);
-void entrada();
-
-unsigned char conv_7seg[17] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07
-                      , 0x7F, 0x6F, 0x77, 0x7F, 0x39, 0x3F, 0x79, 0x71};
-
-void __interrupt (high_priority) f_interrupt_high (void) {
-   
-    if (TMR0IE && TMR0IF) {
-        TMR0L += 6;
-        f_timers();
-        TMR0IF=0;
-    }
-    
-
-    if (ADIE && ADIF) {
-        ADIE = 0;
-        fint_ADC();
-        ADIF=0;     
-    }
-    
-    
-}
-
-void __interrupt (low_priority) f_interrupt_low (void) {
-    
-    
-    
-}
-
-void f_timers(void) {
-    
-    static unsigned int cont_timer0=0, cont_timer1=0, cont_timer2=0, cont_timer3 = 0;
-    static char cont_disp=0;
-    
-    cont_timer3++;
-    
-    TRISB6 = 0;
-    if (LATB6) LATB6=0; else LATB6=1;
-    
-    if (cont_disp < 4) {
-        cont_disp++;
-    }
-    
-    else {
-        cont_disp=0;
-        
-        if (fLCDnew==0) {
-        maqdisplay();
-        keybdisplay();
-        }
-    }
-    
-    
-    if (cont_timer0 < max_timer0) {
-        cont_timer0++;
-    }
-    
-    else {
-        cont_timer0=0;
-        f_timer0();
-    }
-    
-    if (cont_timer1 < max_timer1) {
-        cont_timer1++;
-    }
-    else {
-        cont_timer1=0;
-        f_timer1();
-    }
-    
-    if (cont_timer2 < max_timer2) {
-        cont_timer2++;
-    }
-    
-    else {
-        cont_timer2=0;
-        f_timer2();
-    }
-    
-    if(cont_timer3 == 10)
-    {
-        entrada();
-    }
-    
-}
-
-void setup_hard (void) {
-    
-    IPEN = 0;
-    GIE = 0;
-    
-    // PORTAS ENTRADA E SAIDA
-    
-    ADCON1 = 0b00001111;
-    
-    TRISA = 0b11000011;
-    TRISB = 0b11111111;
-    TRISD = 0b00000000;
-    TRISE = 0b11111001;
-    
-    
-    // INTERRUPCAO
-    
-    
-    //timer0 fosc=4000000 tempo=1e-3
-    T0CON = 0b11010001;
-    TMR0L = 6;
-    
-    // AD  [TADmin = 0,7us  TACQmin = 2,4us  Fosc = 4MHz]
-    
-    ADCON0 = 0b00000001; //AN0
-    ADCON1 = 0b00001110; 
-    ADCON2 = 0b10010100;
-    
-    disp1 = 0; disp2 = 0; disp3 = 0; disp4 = 0;
-    display = conv_7seg[16];
-    
-    lcd_init();
-    
-    PWM_Init();
-    
-    setup(); // SETUP LOGICO
-    
-    
-    // HABILITANDO OU NAO INTERRUPCAO
-    
-    PEIE = 1;
-    GIE = 1;
-}
-
-
-
-void main (void) {
-    
-    setup_hard();
-    while(1) {
-        
-        if(fLCDnew) {
-        mostraLCD();
-        fLCDnew=0; 
-        }
-        
-        loop();
-    }
-    
-}
-
-void maqdisplay (void) {
-    
-    static unsigned char estado = 1;
-    
-    
-    switch (estado) {
-        
-        case 1:
-            estado = 2;
-            disp4 = 0;
-            display = conv_7seg[n1];
-            disp1 = 1;
-            break;
-            
-        case 2:
-            estado = 3;
-            disp1 = 0;
-            display = conv_7seg[n2];
-            disp2 = 1;
-            break;
-            
-        case 3:
-            estado = 4;
-            disp2 = 0;
-            display = conv_7seg[n3];
-            disp3 = 1;
-            break;
-            
-        case 4:
-            estado = 1;
-            disp3 = 0;
-            display = conv_7seg[n4];
-            disp4 = 1;
-            break;
-            
-        default:
-            estado = 1;
-            disp1 = 0; disp2 = 0; disp3 = 0; disp4 = 0;
-            display = conv_7seg[16];
-            
-    }
-    
-}
-
-void keybdisplay (void) {
-    
-    if(linha1 && controle == 0x01)
-    {
-        controle = 0x02;
-        linha1 = 0x00;
-        linha2 = 0x01;
-        linha3 = 0x01;
-        linha4 = 0x01;
-    
-        if(!RB0)
-        {
-            numero = 1;
-        }
-        else if(!RB1)
-        {
-            numero = 2;
-        }
-        
-        else if(!RB2)
-        {
-            numero = 3;
-        }
-    }
-    
-    
-    else if(linha2 && controle == 0x02)
-    {
-        controle = 0x03;
-        linha1 = 0x01;
-        linha2 = 0x00;
-        linha3 = 0x01;
-        linha4 = 0x01;
-        
-        if(!RB0)
-        {
-            numero = 4;
-        }
-        else if(!RB1)
-        {
-            numero = 5;
-        }
-        
-        else if(!RB2)
-        {
-            numero = 6;
-        }
-    }
-    
-    else if(linha3 && controle == 0x03)
-    {
-        controle = 0x04;
-        linha1 = 0x01;
-        linha2 = 0x01;
-        linha3 = 0x00;
-        linha4 = 0x01;
-        
-        if(!RB0)
-        {
-            numero = 7;
-        }
-        else if(!RB1)
-        {
-            numero = 8;
-        }
-        
-        else if(!RB2)
-        {
-            numero = 9;
-        }
-        
-        
-    }
-    
-    else if(linha4 && controle == 0x04)
-    {
-        controle = 0x01;
-        linha1 = 0x01;
-        linha2 = 0x01;
-        linha3 = 0x01;
-        linha4 = 0x00;
-        
-        if(!RB0)
-        {
-            numero = 0x2a;      //'*'   42Dec
-        }
-        else if(!RB1)
-        {
-            numero = 0;
-        }
-        
-        else if(!RB2)
-        {
-            numero = 0x23;      //'#'   35Dec
-        }
-    }
-    
-}
-
-
-// PARTE LOGICA
-
-
-void setup (void) {
-    
-    max_timer0 = 50 - 1;
-    max_timer1 = 200 - 1; // TEMPO DE AMOSTRAGEM DO ESTADO
-    max_timer2 = 200 - 1;
-    
-    
-    strcpy(linha01, "MARC_MATH_RODR");
-    
-}
-
-void loop (void) {     
-    valorADC = lerADC();
-    if (fADCnew) {
-    
-        fADCnew=0;
-   
-    }
-}
-
-void startADC ( void) {
-    
-    ADIF = 0;
-    ADIE = 1;
-    GO = 1;
-    
-}
-
-int lerADC (void) {
-    
-    int valor = 0;
-    valor = ADRESH;
-    valor = valor<<8;
-    valor += ADRESL; 
-                                            
-    return valor;
-    
-}
-
-void mostraLCD (void) {
-    
-    extern char linha01[17], linha02[17];
-    
-    disp1 = 0; disp2 = 0; disp3 = 0; disp4 = 0;
-    
-    lcd_cmd(L_CLR);
-    
-    lcd_cmd(L_L1);
-    lcd_str(linha01);
-    
-    lcd_cmd(L_L2);
-    lcd_str(linha02);
-      
-    display = conv_7seg[16];
-    
-}
-
-void f_int0 (void) 
-{     
-    fan = 0;
-    PWM1_Set_Duty(fan);
-}
-
-
-void f_int1 (void) {
-    
-    fan = 500;
-    PWM1_Set_Duty(fan);
-}
-
-
-void f_int2 (void) {
-
-}
-
-
-void f_timer0 (void) {
-    startADC();
-     
-}
-
-
-void f_timer1 (void) {
-    
-    
-}
-
-
-void f_timer2 (void) {
-    
-    
-    
-}
-
-
-void fint_ADC (void) {
-    
-    valorADC = lerADC();
-    fADCnew=1;
-    
-}
-
-void entrada()
-{
-    static unsigned char rb3_old=1, rb4_old=1, rb5_old=1;
-    
-    unsigned char rb3_new, rb4_new, rb5_new;
-    
-    // amostragem dos pinos de entrada
-    rb3_new = RB3;
-    rb4_new = RB4;
-    rb5_new = RB5;
-    
-    //analise
-    
-    if (rb3_new<rb3_old) f_int2();
-    if (rb4_new<rb4_old) f_int1();
-    if (rb5_new<rb5_old) f_int0();
-    
-    //quardo o valor atual
-    
-    rb3_old = rb3_new;
-    rb4_old = rb4_new;
-    rb5_old = rb5_new;
-}*/
-
 // aula do dia 15/09/2021
 //pwm
 
@@ -509,9 +29,8 @@ void f_timer2 (void);
 void f_timer3 (void);
 void fint_ADC (void);
 void escreve_DC (float valor_dc);
-
 void mostraLCD (void);
-
+void password();
 void startADC (void);
 int lerADC (void);
 
@@ -530,7 +49,13 @@ unsigned char fLCDnew=0;
 unsigned char n1=1, n2=2, n3=3, n4=4;
 unsigned int valorAD = 0;
 unsigned int max_timer0, max_timer1, max_timer2, max_timer3;
-unsigned int dado_teclado;
+unsigned int dado_teclado = 0;
+unsigned int star_lcd = 0;
+unsigned int dado = 0;
+unsigned int dado_v [4] = {0,0,0,0};
+char flag = 1;
+static int dado_teclado_old = 0;
+static unsigned int count = 0;
 unsigned char conv_7seg[17] = {
     0b00111111,
     0b00000110,
@@ -704,7 +229,7 @@ void setup_hard (void) {
 
 
 void main (void) {
-    
+    int erase = 3;
     setup_hard();
     while(1) {
         
@@ -712,23 +237,31 @@ void main (void) {
         {
             if(dado_teclado == 11)
             {
-                strcpy(linha2, "");
+                dado = dado / 10;
+                count = 3;
+                sprintf(linha2, "%d", dado); 
             }
             
             else if(dado_teclado == 12)
             {
-                strcpy(linha1, "");
                 strcpy(linha2, "");
+                dado = 0;
+                for(int i = 0; i < 4; i++)
+                {
+                    dado_v[i] = 0;
+                }
+                count = 0;
             }
             
             else
             {
-                strcpy(linha1, "MARC_MATH_RODR");
+                strcpy(linha1, "MARC_MATH_RODR"); 
+                sprintf(linha2, "%d", dado); 
             }
             mostraLCD();
             fLCDnew=0; 
         }
-        
+        atraso(10);
         loop();
     }
     
@@ -875,6 +408,7 @@ void setup (void) {
     n1 = 0; n2 = 0; n3 = 0; n4 = 0;
     
     strcpy(linha1, "MARC_MATH_RODR");
+    strcpy(linha2, "PWM");
 }
 
 
@@ -900,67 +434,43 @@ void f_int1 (void) {
 
 void f_int2 (void) {
        
-    //escreve_DC(dado_teclado);
-    switch(dado_teclado)
-    {
-        case 1:
-            PWM1_Set_Duty(0);
-            break;
-        
-        case 2:
-            PWM1_Set_Duty(113);
-            break;
-        
-        case 3:
-            PWM1_Set_Duty(226);
-            break;
-            
-        case 4:
-            PWM1_Set_Duty(339);
-            break;
-            
-        case 5:
-            PWM1_Set_Duty(452);
-            break;
-            
-        case 6:
-            PWM1_Set_Duty(565);
-            break;
-            
-        case 7:
-            PWM1_Set_Duty(678);
-            break;
-            
-        case 8:
-            PWM1_Set_Duty(791);
-            break;
-            
-        case 9:
-            PWM1_Set_Duty(904);
-            break;
-        
-        case 0:
-            PWM1_Set_Duty(1023);
-            break;
-    }
+
+    PWM1_Set_Duty(dado);
     
 }
 
 
-void f_timer0 (void) {
-    static unsigned int dado_teclado_old = 0;
-    
+void f_timer0 (void) {    
     startADC();
-    
     entrada();
     
     dado_teclado=ler_teclado();
-    if(dado_teclado != dado_teclado_old)
+    if(flag && (dado_teclado!=dado_teclado_old))
     {
-        fLCDnew = 1;
         dado_teclado_old = dado_teclado;
-        sprintf(linha2, "%2d", dado_teclado);
+        if(dado_teclado != 11 && dado_teclado != 12)
+        {
+            password();
+        }
+        fLCDnew = 1;
     }
+    
+    /*else if(PORTD && !flag)
+    {
+        flag = 1;
+        //dado_teclado_old = 0;
+    }*/
+    
+    if(star_lcd && !flag)
+    {
+
+        star_lcd = 0;
+        flag = 1;
+        dado_teclado_old = -2;
+    }
+    
+
+    
     //mostra7seg(dado_teclado);
 }
 
@@ -971,7 +481,7 @@ void f_timer1 (void) {
 
 
 void f_timer2 (void) {
-       
+
 }
 
 
@@ -1026,38 +536,122 @@ unsigned char ler_teclado(void) {
     for (int i=0; i<10; i++) Nop();
     entrada = PORTD;
     
-    if ((entrada & 1) == 0) saida=11;
-    if ((entrada & 2) == 0) saida=7;
-    if ((entrada & 4) == 0) saida=4;
-    if ((entrada & 8) == 0) saida=1;
+    if ((entrada & 1) == 0)
+    { 
+        saida=11;
+        star_lcd = 1; 
+        flag = 0;
+    }
+    if ((entrada & 2) == 0)
+    {
+        saida=7;
+        star_lcd = 1;
+        flag = 0;
+    }
+    if ((entrada & 4) == 0)
+    {
+        saida=4;
+        star_lcd = 1;
+        flag = 0;
+    }
+    if ((entrada & 8) == 0)
+    { 
+        saida=1;
+        star_lcd = 1;
+        flag = 0;
+    }
     
     LATB ^= 0b00000011;
     for (int i=0; i<10; i++) Nop();
     entrada = PORTD;
     
-    if ((entrada & 1) == 0) saida=0;
-    if ((entrada & 2) == 0) saida=8;
-    if ((entrada & 4) == 0) saida=5;
-    if ((entrada & 8) == 0) saida=2;
-    
+    if ((entrada & 1) == 0) 
+    {
+        saida=0;
+        star_lcd = 1;
+        flag = 0;
+    }
+    if ((entrada & 2) == 0) 
+    {
+        saida=8;
+        star_lcd = 1;
+        flag = 0;
+    }
+    if ((entrada & 4) == 0)
+    {
+        saida=5;
+        star_lcd = 1;
+        flag = 0;
+    }
+    if ((entrada & 8) == 0)
+    { 
+        saida=2;
+        star_lcd = 1;
+        flag = 0;
+    }
     LATB ^= 0b00000110;
     for (int i=0; i<10; i++) Nop();
     entrada = PORTD;
     
-    if ((entrada & 1) == 0) saida=12;
-    if ((entrada & 2) == 0) saida=9;
-    if ((entrada & 4) == 0) saida=6;
-    if ((entrada & 8) == 0) saida=3;
-    
+    if ((entrada & 1) == 0) 
+    {
+        saida=12;
+        star_lcd = 1;
+        flag = 0;
+    }
+    if ((entrada & 2) == 0) 
+    {
+        saida=9;
+        star_lcd = 1;
+        flag = 0;
+    }
+    if ((entrada & 4) == 0) 
+    {
+        saida=6;
+        star_lcd = 1;
+        flag = 0;
+    }
+    if ((entrada & 8) == 0) 
+    {
+        saida=3;
+        star_lcd = 1;
+        flag = 0;
+    }
     TRISB = 0b11111111;
     TRISD = 0b00000000;
        
     return saida;
 }
 
+void password()
+{
+    if(dado_teclado!=11 || dado_teclado!=12){
+        switch (count)
+        {
+            case 0:
+                count = 1;
+                dado_v[0] = dado_teclado*1000;
+                break;
 
+            case 1:
+                count = 2;
+                dado_v[1] = dado_teclado*100;
+                break;
 
-
-
-
-
+            case 2:
+                count = 3;
+                dado_v[2] = dado_teclado*10;
+                break;
+               
+            case 3:
+                count = 0;
+                dado_v[3] = dado_teclado;
+                break;
+        }
+        dado = dado_v[0] + dado_v[1] + dado_v[2] + dado_v[3];         
+    }
+    
+    
+    
+    
+}
