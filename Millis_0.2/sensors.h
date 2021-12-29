5f36274e58cc675667045d6aad85739bec8fdcf2
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2021-12-29 19:00:25 -0300

    Update sensors.h

--- Millis_0.2/sensors.h
+++ Millis_0.2/sensors.h
@@ -9,4 +9,5 @@
 void sensors_ADC_maq();
 void sensors_le_marcadores(void);
 void sensors_sentido_de_giro();
+void sensors_leitura_de_pista(int *erro_sensores, int *speedW, int *speedX, unsigned int *PWM_general, unsigned int *PWMR, unsigned int *PWM_Curva);
 //void sensors_volta_pra_pista(void);
