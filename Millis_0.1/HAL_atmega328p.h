a569e5a812676c85a1cbc0ab2f301c2fecafc025
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2022-01-27 15:19:06 -0300

    Update HAL_atmega328p.h

--- Millis_0.1/HAL_atmega328p.h
+++ Millis_0.1/HAL_atmega328p.h
@@ -3,6 +3,9 @@
 #include <avr/io.h>           //Biblioteca geral dos AVR
 #include <avr/interrupt.h>    //Biblioteca de interrupção
 #include <util/delay.h>
+#include "ADC.h"
+#include "PWM.h"
+#include "UART.h"
 
 //variáveis de comando para os registradores
 #define set_bit(y,bit) (y|=(1<<bit)) //coloca em 1 o bit x da variável Y
@@ -12,45 +15,40 @@
 /*==============================================================*/
 
 /*Mapeamento de Hardware*/
-#define sensor_de_curva   PB0
-#define sensor_de_parada  PD7
-#define led               PB5
-#define leitura_curva    PINB
-#define leitura_parada   PIND
-
-extern void setFreq(char option);
-extern void ADC_init();
-void INT_INIT();
-extern void UART_config(unsigned int ubrr);
-
-/*tempo =65536 * Prescaler/Fosc = 65536 * 1024/16000000 = 4, 19s
- tempo = X_bit_timer * Prescaler/Fosc
- Valor inicial de contagem = 256 - tempo_desejado*Fosc/Prescaler = 256 - 0,001*16000000/1024 = 255
- Valor inicial de contagem = X_bit_timer - tempo_desejado*Fosc/Prescaler */
-/*===========================================================================*/
+#define sensor_de_curva          PB3
+#define sensor_de_parada         PB4
+#define led_placa                PB5
+#define led_offboard             PC5
+#define leitura_sensores        PINB
+#define encoder_C1E              PB0
+#define encoder_C2E              PB5
+#define encoder_C1D              PD2
+#define encoder_C2D              PD7
+#define leitura_outros_sensores PIND
 
 
-/*void setup_Hardware()
+void setup_Hardware()
 {
     MCUCR &= 0xef;      //habilita pull up quando configurado e desabilita algumas configurações prévias do MCU
 
-    DDRD = 0b01111010; //PD3 - PD6 definidos como saída, PD7 como entrada, PD0 como entrada(RX) e PD1 como saída(TX)
-    PORTD = 0b10000000; //inicializados em nível baixo e PD7 com pull up
-    DDRB = 0b00100110; //Habilita PB0 como entrada e PB5, PB1 e PB2 como saída
-    PORTB = 0b00000001; //PORTB inicializa desligado e pull up no PB0
-    DDRC = 0b00000000; //PORTC como entrada
-    PORTC = 0b00001111; //PC3 - PC0 com pull up (colocar resistor de pull up nos pinos A6 e A7)
+    DDRD  = 0b01111010;     //PD0, PD2 e PD7 como entrada, demais como saída
+    PORTD = 0b00000000;     //todas as saídas iniciam em 0 e entradas sem pull-up
+    DDRB  = 0b00000110;     //PB1 e PB2 como saída e demais como entrada
+    PORTB = 0b00011000;     //PB3 e PB4 com pull up ativado
+    DDRC  = 0b00100000;     //PC5 como saída
+    PORTC = 0b00011111;     //PC0-PC4 com pull-up ativado   
     TCCR1A = 0xA2; //Configura operação em fast PWM, utilizando registradores OCR1x para comparação
     //TCCR1A = 0xA3 //Configura em phase correct PWM OC1A e OC1B limpos
     
     //esquerdo pino 4 - PD2
     UART_config(16); //Inicializa a comunicação UART com 57.6kbps
     
-    setFreq(4); //Seleciona opção para frequência
+    PWM_setFreq(4); //Seleciona opção para frequência
     //62,5Hz de PWM
     
     ADC_init(); //Inicializa o AD
     INT_INIT(); //Inicializa o Timer0
+    EXT_INIT(); //Inicializo as interrupções externas
 
 }//end setup_hardware
 
@@ -59,4 +57,12 @@ void INT_INIT()
     TCCR0B = 0b00000010; //TC0 com prescaler de 8
     TCNT0 = 56; //Inicia a contagem em 56 para, no final, gerar 100us
     TIMSK0 = 0b00000001; //habilita a interrupção do TC0
-}*/
+}
+
+void EXT_INIT()
+{
+    EICRA  = 0b00000010; //habilito a interrupção do INT0 por qualquer mudança de estado
+    EIMSK  = 0b00000001; //habilito a interrupção externa do INT0
+    PCICR  = 0b00000001; //habilito a interrupção externa do canal PCINT0
+    PCMSK0 = 0b00000001; //habilito a porta PCINT0 como interrupção externa do canal PCINT0
+}
