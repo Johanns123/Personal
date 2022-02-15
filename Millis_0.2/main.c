bf013491fd137a2a08c7c252a288e0c421d0fe91
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2022-02-15 10:04:47 -0300

    att. maic
    algumas perfumarias e inclusão de variáveis que precisam receber valores antes de iniciar a rotina

--- Millis_0.2/main.c
+++ Millis_0.2/main.c
@@ -3,61 +3,74 @@
  * Author: Johann
  *
  * Created on 30 de Maio de 2021, 16:52
- * Last update on December 23td of 2021 at 17:55
  */
 
-/*Este código é de um robô seguidor de linha da equipe Wolfbotz.
- * Aqui nós vemos o controle do robô bem como as tomadas de decisão de acordo com os padrões da pista*/
+/* Este codigo eh de um robo seguidor de linha da equipe Wolfbotz.
+ * Aqui nos vemos o controle do robo bem como as tomadas de decisao de acordo com os padroes da pista
+ */
+
+#define TOMADA_DE_TEMPO 1
+#define MAPEAMENTO 2
+
+#define numero_de_trechos 10
+
 #include "main.h"
 
-/*Variáveis globais*/
-unsigned int PWMA = 0, PWMB = 0; // Modulação de largura de pulso enviada pelo PID
-unsigned int PWMR;                               // PWM do motor ao entrar na reta
-unsigned int PWM_Curva;                          // PWM do motor ao entrar na curva
-//variáveis de controle
-bool f_parada= 0;       //variável que comanda quando o robô deve parar e não realizar mais sua rotina
-bool flag = 0;          //variável de controle para identificar o momento de parada
-bool flag_curva = 0;    //cronometragem entre as retas e as cruvas
-bool flag_parada = 0;   //inicia e encerra a cronometragem da pista
-bool f_stop = 0;              //encerra a rotina de dados
-unsigned int millisegundos = 0;        //millisegundos
-unsigned char vect_duty_cycle[101];
-unsigned int vect_PWM[512];
+/*Variaveis globais*/
+unsigned int PWMA = 0, PWMB = 0; // Modulacao de largura de pulso enviada pelo PID
+unsigned int PWMR;               // PWM do motor ao entrar na reta
+unsigned int PWM_Curva;          // PWM do motor ao entrar na curva
 
-unsigned char SW;   //switch de estratégia
+//variaveis de controle
+bool f_parada    = 0;   // variaavel que comanda quando o robo deve parar e nao realizar mais sua rotina
+bool flag        = 0;   // variavel de controle para identificar o momento de parada
+bool flag_curva  = 0;   // cronometragem entre as retas e as cruvas
+bool flag_parada = 0;   // inicia e encerra a cronometragem da pista
+bool f_stop      = 0;   // encerra a rotina de dados
+
+unsigned int  millisegundos = 0;     // millisegundos
+unsigned char vect_duty_cycle[101];  // valores de duty_cycle
+unsigned int  vect_PWM[512];         // valroes de PWM
+
+unsigned char SW;               // switch de estrategia
 unsigned char estrategia = 0;
 
-/*Variáveis do encoder*/
-unsigned char pulse_numberR = 0, pulse_numberL = 0; //variáveis para contagem dos pulsos dos encoders
+/*Variaveis do encoder*/
+unsigned char pulse_numberR = 0, 
+              pulse_numberL = 0; // variaveis para contagem dos pulsos dos encoders
 
-/*Variáveis da UART*/
-volatile char ch; //armazena o caractere lido
-volatile char flag_com = 0; //flag que indica se houve recepção de dado*/
+/*Variaveis da UART*/
+volatile char ch;           // armazena o caractere lido
+volatile char flag_com = 0; // flag que indica se houve recepcao de dado*/
 
 unsigned char max_timer1, max_timer2, max_timer3_ms, max_timer_3, max_timer4, max_timer5;   
-
-
 int  u_X = 0;
 
-/*Interrupções*/
-ISR(USART_RX_vect) {
-    ch = UDR0; //Faz a leitura do buffer da serial
 
-    UART_enviaCaractere(ch); //Envia o caractere lido para o computador
-    flag_com = 1; //Aciona o flag de comunicação
+/*Variáveis usadas na Running*/
+unsigned char MaxSpeed[numero_de_trechos] = {0};             // velocidade máxima de cada trecho
+unsigned char distancia_total[numero_de_trechos] = {0};    // dados encontrados no mapeamento
+unsigned char valor_de_tolerancia[numero_de_trechos] = {0};  // distancia de tolerancia para o robô começar a frear
+
+/*Estes valores serao inseridos manualmente apos o mapeamento*/
+
+/*Interrupcoes*/
+ISR(USART_RX_vect) {
+    ch = UDR0;               // Faz a leitura do buffer da serial
+    UART_enviaCaractere(ch); // Envia o caractere lido para o computador
+    flag_com = 1;            // Aciona o flag de comunicacao
 }
 
 ISR(TIMER0_OVF_vect) 
 {
-    TCNT0 = 56; //Recarrega o Timer 0 para que a contagem seja 100us novamente
-    
-    f_timers(); //função de temporização das rotinas   
-}//end TIMER_0
+    TCNT0 = 56; // recarrega o Timer 0 para que a contagem seja 100us novamente
+    f_timers(); // funcao de temporizacao das rotinas   
+}
 
 ISR(ADC_vect)
 {
-    sensors_ADC_maq();  //máquina de estado do conversor AD
-}//end ADC_int
+    sensors_ADC_maq();  // maquina de estado do conversor AD
+}
 
 
 ISR(INT0_vect)
@@ -65,24 +78,29 @@ ISR(INT0_vect)
     count_pulsesD();
 }
 
+
 ISR(PCINT0_vect)
 {
     count_pulsesE();
 }
 
 
-/*Função principal*/
+// Funcao principal
 int main(void) 
 {
     setup();
     while (1) loop();
-    return 0;
-}//end main
+    return 0;    
+}
 
-/*RTOS primitivo*/
+// ======================================================================================================
+// ===================================== RTOS primitivo =================================================
+// ======================================================================================================
+// ============================== Parte nao visivel ao usuario ==========================================
+// ======================================================================================================
 
-/*Parte não visível ao usuário*/
-void f_timers (void)//100us 
+
+void f_timers (void) /* Funcao e chamada a cada 100us */ 
 {
     static unsigned char c_timer1 = 1;
     static unsigned char c_timer2 = 1;
@@ -90,272 +108,226 @@ void f_timers (void)//100us
     static unsigned char c_timer4 = 1;
     static unsigned char c_timer5 = 1;
         
-    //funções a cada 200us
-    if(c_timer1 < max_timer1)
-    {
-        c_timer1++;
-    }
-
+    /* funcoes a cada 200us | max_timer1 = 2 */
+    if(c_timer1 < max_timer1) c_timer1++;
     else
     {
         f_timer1();
         c_timer1 = 1;
     }
 
-    /*300us*/
-    if (c_timer2 < max_timer2)
-    {
-        c_timer2++; //100us -1; 200us-2;300 us-3; 300us de intervalo de tempo
-    }
-
-    else    //a cada 300us
+    /* funcoes a cada 300us | max_tiemr2 = 3 */
+    // 100us -1; 200us-2;300 us-3; 300us de intervalo de tempo
+    if (c_timer2 < max_timer2) c_timer2++;
+    else 
     {
         f_timer2();
         c_timer2 = 1;
     }
-
-    if(c_timer3 < max_timer3_ms)   //10ms
-    {
-        c_timer3++;
-    }
-
+    
+    /* funcoes a cada 10ms | max_timer3_ms = 100 */
+    if(c_timer3 < max_timer3_ms) c_timer3++;
     else
     {
         f_timer3();
         c_timer3 = 1;
     }
 
-    //Timer
-    //1ms
-    if(c_timer4 < max_timer4)
-    {
-        c_timer4++;
-    }
-
+    /* funcoes a cada 1ms | max_timer4 = 10 */
+    if(c_timer4 < max_timer4) c_timer4++;
     else
     {
         f_timer4();
         c_timer4 = 1;
     }
     
-    if(c_timer5 < max_timer5)   //1ms
-    {
-        c_timer5++;
-    }
-
+    /* funcoes a cada 1ms | max_timer5 = 10 */
+    if(c_timer5 < max_timer5) c_timer5++;
     else
     {
         f_timer5();
         c_timer5 = 1;
-    }
-    
-}//fim do RTOS
+    }    
+}
 
-
-//===Funções não visíveis ao usuário======//
+// ======================================================================================================
+// =========================== Funcoes nao viriaveis ao usuario =========================================
+// ======================================================================================================
 void setup() 
 {
+    disable_interrupts();    // desabilita todas as interrupcoes
+    setup_Hardware();       // setup das IO's e das interrupcoes
+    enable_interrupts();   // habilita todas as interrupcoes
+    setup_variaveis();    // definicao das variaveis logicas
+}
 
-    setup_Hardware();   //setup das IO's e das interrupções
-    sei();              //Habilita as interrupções
-    setup_logica();     //definição das variáveis lógicas
-
-}//end setup
-
-void setup_logica() /*Função que passa ponteiros para funções como parâm*/
-{
-    
-    
-    max_timer1 = 2,
-    max_timer2 = 3,
+void setup_variaveis()
+{    
+    // Funcao que passa ponteiros para funcoes como parametro 
+    max_timer1    = 2,
+    max_timer2    = 3,
     max_timer3_ms = 100,
-    max_timer_3 = 50, 
-    max_timer4 = 10,
-    max_timer5 = 10;   
+    max_timer_3   = 50, 
+    max_timer4    = 10,
+    max_timer5    = 10;   
     
-    /*Conversão de PWM para duty*/
+    /* Conversao de PWM para duty */
     for(int i = 0; i < 101; i++)
     {
         vect_duty_cycle[i] = i*(511/100);
     }
     
-    /*Conversão de duty para PWM*/
+    /* Conversao de duty para PWM */
     for(int j = 0; j < 512; j++)
     {
         vect_PWM[j] = j*(100/511);
-    }
-    
+    }    
     if(!SW)
     {
-        estrategia = 1; //tomada de tempo
+        estrategia  = TOMADA_DE_TEMPO;
         PWMR        = PWM_calc_pwm(40);  // duty: 40%
         PWM_Curva   = PWM_calc_pwm(30);  // duty: 30%    
-    }
-    
+    }    
     else
     {
-        estrategia = 0; //mapeamento
+        estrategia  = MAPEAMENTO;
         PWMR        = PWM_calc_pwm(10);  // duty: 10%
         PWM_Curva   = PWM_calc_pwm( 8);  // duty: 8%
     }
+    
 }
 
 
-void loop()//loop vazio
-{
-
-}
-
-void tomada_de_tempo()
-{
-
-    if (!f_parada)  //se f_parada for 0... 
-    {
-        sensors_sentido_de_giro();      //Verifica se precisa fazer uma curva e o cálculo do PID
-        Running_Torricielli();
-    } 
-}
-
-void mapeamento()
-{
-    if (f_parada)  //se f_parada for 1... 
-    {
-        sensors_sentido_de_giro();      //Verifica se precisa fazer uma curva e o cálculo do PID
-    } 
-}
+void loop() {} // loop vazio
 
 
 void parada() 
 {     
-    sensors_laterais();
+    sensors_laterais(); // faz a leitura dos sensores laterais e ativa as respectivas flags
+    fim_de_pista();     // verifica se e o fim da pista, se verdadeiro = motor_off
 }
 
 
 void fim_de_pista()
 {
-    static char parada = 0;
-    
+    static char parada = 0;    
     if(flag)
     {
        parada++;
        flag = 0;
     }
-    
-    
-    if(parada > 1)  //dois marcadores de parada
+    if(parada > 1)  // dois marcadores de parada = fim de pista
     {
         f_parada = 1;
+        parada   = 0;
         motores_freio();
-        parada = 0;
     } 
 }
 
-/*funções do encoder*/
 
+void tratar_estrategia(void) 
+{   
+    // Decisao em funcao da tomada de tempo ou mapeamento    
+    if (f_parada) return;
+    sensors_sentido_de_giro();
+    if (estrategia == TOMADA_DE_TEMPO)
+    {
+        Running_Torricielli();
+    }
+}
+
+
+/*funcoes do encoder*/
 void count_pulsesD() 
 {
     static bool direction_m;
     static bool Encoder_C1Last = 0;
-
-    bool Lstate = (tst_bit(leitura_outros_sensores, encoder_C1D) >> encoder_C1D); //variável de leitura de um dos pinos do encoderD
-
-    if (!Encoder_C1Last && Lstate) {
-        //Verifica se Encoder_C1Last é falso e Lstate é verdadeiro
-        bool val = tst_bit(leitura_outros_sensores, encoder_C2D >> encoder_C2D); //Variável de leitura do segundo pino do encoderD
-
-        if (!val && direction_m) direction_m = 0; //sentido horário
-
-        else if (val && !direction_m) direction_m = 1; //sentido anti-horário
+    bool Lstate = (tst_bit(leitura_outros_sensores, encoder_C1D) >> encoder_C1D); // variavel de leitura de um dos pinos do encoderD
+    if (!Encoder_C1Last && Lstate) 
+    {
+        // Verifica se Encoder_C1Last eh falso e Lstate e verdadeiro
+        bool val = tst_bit(leitura_outros_sensores, encoder_C2D >> encoder_C2D); // variavel de leitura do segundo pino do encoderD
+        if (!val && direction_m) direction_m = 0;       // sentido horario
+        else if (val && !direction_m) direction_m = 1;  // sentido anti-horario
     }
-
     Encoder_C1Last = Lstate;
+    if (!direction_m) pulse_numberR++; //sentido horario
+    else pulse_numberR--;              //sentido anti-horario
 
-    if (!direction_m) pulse_numberR++; //sentido horário
-    else pulse_numberR--;
 }
+
 
 void count_pulsesE()
 {
     static bool direction_m;
     static bool Encoder_C1Last = 0;
-
-    bool Lstate = (tst_bit(leitura_sensores, encoder_C1E) >> encoder_C1E); //variável de leitura de um dos pinos do encoderD
-
+    bool Lstate = (tst_bit(leitura_sensores, encoder_C1E) >> encoder_C1E); //variavel de leitura de um dos pinos do encoderE
     if (!Encoder_C1Last && Lstate) 
-    { //Verifica se Encoder_C1Last é falso e Lstate é verdadeiro
-        bool val = (tst_bit(leitura_sensores, encoder_C2E) >> encoder_C2E); //Variável de leitura do segundo pino do encoderD
-
-        if (!val && direction_m) direction_m = 0; //sentido horário
-
-        else if (val && !direction_m) direction_m = 1; //sentido anti-horário
+    { 
+        //Verifica se Encoder_C1Last eh falso e Lstate eh verdadeiro
+        bool val = (tst_bit(leitura_sensores, encoder_C2E) >> encoder_C2E); //Variavel de leitura do segundo pino do encoderE
+        if (!val && direction_m) direction_m = 0;       //sentido horario
+        else if (val && !direction_m) direction_m = 1;  //sentido anti-horario
     }
-
     Encoder_C1Last = Lstate;
-
-    if (!direction_m) pulse_numberL++; //sentido horário
-    else pulse_numberL--; //sentido anti-horário
+    if (!direction_m) pulse_numberL++; //sentido horario
+    else pulse_numberL--;              //sentido anti-horario
 }
-
-
 
 void millis(void)
 {
-    //static unsigned int f_read = 0;
-    
+    // static unsigned int f_read = 0;    
     millisegundos++;
 }
 
-void f_timer1(void) //200us
+
+/* tempo = 200us */
+void f_timer1(void) 
 {
-    parada();
-    fim_de_pista();         //Verifica se é o fim da pista
+    parada();       // analisa as condicoes dos sensores laterais de acordo com suas leituras    
 }
 
-void f_timer2(void) //300us
+/* tempo = 300us */
+void f_timer2(void)
 {
-    if(estrategia)
-    {
-        tomada_de_tempo();
-    }
-    
-    else
-    {
-        mapeamento();
-    }
+    tratar_estrategia();
 }
 
-void f_timer3(void)     //10ms
+/* tempo = 10ms */
+void f_timer3(void)
 {   
-    static unsigned char c_timer1 = 0;
-    
-    if(c_timer1 < max_timer_3)  //500ms = 0,5s
-    {
-        c_timer1++;
-    }
-
-    else 
-    {   
-        if(!estrategia)
-        {
-            dados_telemetria(); //mapeamento
-        }
-        c_timer1 = 1;
-    }
+    sub_timer();
 }
 
+/* tempo = 1ms */
 void f_timer4(void)
 {
-    millis();   //função chamada a cada 1ms 
-
+    millis();   // funcao chamada a cada 1ms 
 }
 
+
+/* tempo = 1ms */
 void f_timer5(void)
 {
-    if(!f_stop)
-    {   
-        if(!estrategia) //mapeamento
-        {   
-            dados_coleta();
-        }
-    }
+    if(f_stop) return;    
+    if(estrategia == MAPEAMENTO) dados_coleta();
 }
+
+void sub_timer(void)/*sub_timer em que conta uma função temporizada a cada 0,5s*/
+{
+    static unsigned char c_timer1 = 0;
+    
+    /* tempo = 500ms ou 0,5 segundos | max_timer_3 = 50 */
+    if(c_timer1 < max_timer_3) c_timer1++;
+    
+    else 
+    {   
+        if(estrategia == MAPEAMENTO) dados_telemetria();
+        c_timer1 = 1;
+    }
+
+}
+
+// ======================================================================================================
+// ===================================== FINAL DO CODIGO ================================================
+// ======================================================================================================
--- Millis_0.2/main.h
+++ Millis_0.2/main.h
@@ -1,35 +1,36 @@
 #include <stdio.h>            //Bilioteca do C
-#include <stdbool.h>          //Biblioteca que permite utilizar vari�vel booleana
-#include "UART.h"             //Biblioteca da comunicação UART
+#include <stdbool.h>          //Biblioteca que permite utilizar variavel booleana
+#include "UART.h"             //Biblioteca da comunicacao UART
 #include "ADC.h"              //Biblioteca do conversor AD
-#include "PWM.h"      //Biblioteca de PWM fast mode de 10 bits
-#include "motores.h"     //Biblioteca das funções de controle dos motores  //usado para ponte H tb6612fng
+#include "PWM.h"              //Biblioteca de PWM fast mode de 10 bits
+#include "motores.h"          //Biblioteca das funcoes de controle dos motores  //usado para ponte H tb6612fng
 #include "PID.h"              //Biblioteca do controle PID
-#include "sensors.h"     //lógica utilizando os sensores
-#include "dados.h"            //biblioteca que contém as funções atraladas ao envio de informações via UART
-#include "HAL_atmega328p.h"
+#include "sensors.h"          //Logica utilizando os sensores
+#include "dados.h"            //Biblioteca que contem as funcoes atreladas ao envio de informacoes via UART
+#include "HAL_atmega328p.h"   //Biblioteca que configura os registradores e portas do uC ATMEGA328P
 #include "Running.h"
 
-/*Protótipo das funções*/
+/*Prototipo das funcoes*/
 void setup();
-void setup_logica();        //variáveis utilizadas na lógica
+void setup_variaveis();        //variaveis utilizadas na logica
 void loop();
-void tomada_de_tempo();          //estrategia de tomada de tempo
+void tomada_de_tempo();     //estrategia de tomada de tempo
 void mapeamento();          //estrategia de mapeamento
-//---------------------------------------------------------------//
+//---------------------------------------------------------------------//
 void parada();              //Leitura dos sensores laterais
-void fim_de_pista();        //verifica se é o fim da pista 
+void fim_de_pista();        //verifica se e o fim da pista 
 //---------------------------------------------------------------------//
 void count_pulsesE();
 void count_pulsesD();
 void millis(void);
 //---------------------------------------------------------------------//
-void f_timers (void);       //função de temporização das rotinas
+void f_timers (void);       //funcao de temporizacao das rotinas
 void f_timer1(void);
 void f_timer2(void);
 void f_timer3(void);
 void f_timer4(void);
 void f_timer5(void);
+void sub_timer(void);
 /*===========================================================================*/
 
 /*Macros*/
