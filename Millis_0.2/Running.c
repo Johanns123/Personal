41861da77b78ef1b01a6f38515d4a7cfc2c1f477
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2022-02-15 10:06:55 -0300

    att. Running
    Inclusão da chamada de função que calcula a velocidade atual e o redirecionamento de vetores para a main.

--- Millis_0.2/Running.c
+++ Millis_0.2/Running.c
@@ -5,30 +5,31 @@
  * Created on 4 de Fevereiro de 2022, 17:15
  */
 #include "Running.h"
+#include "dados.h"      // verificar depois o uso dessa biblioteca em running.c
 
 /*Em desenvolvimento*/
 void Running_Torricielli(void)
 {
-    #define numero_de_trechos 10
+    #define trechos_totais 10
     extern bool flag_curva;
-    static unsigned char trecho = 0;    //indica qual trecho o robo está
-    static unsigned char distancia_atual      = 0,                  //dados encontrados pelos enconders
-                         distancia_total[numero_de_trechos],    //dados encontrados no mapeamento
+    static unsigned char trecho = 0;                            // indica qual trecho o robo está
+    static unsigned char distancia_atual      = 0,              // dados encontrados pelos enconders
                          distancia_restante   = 0,
-                         distancia_necessaria = 0,
-                         valor_de_tolerancia[numero_de_trechos];  //distancia de tolerancia para o robô começar a frear
+                         distancia_necessaria = 0;
 
-    static unsigned char v_atual = 0, v_final = 0, a = 0;   //velocidades e acelerações
+    static unsigned char v_atual = 0, v_final = 0, a = 0;         // velocidades e acelerações
     
-    static unsigned char MaxSpeed[numero_de_trechos];    //velocidade máxima de cada trecho
-    
-    //v_atual = velocidade medida pelo encoder
+    extern unsigned char distancia_total[trechos_totais];
+    extern unsigned char MaxSpeed[trechos_totais];
+    extern unsigned char valor_de_tolerancia[trechos_totais];  // distancia de tolerancia para o robô começar a frear
+
+    v_atual = dados_velocid_linear();   //velocidade atual
     v_final = MaxSpeed[trecho]; //guardada pelo mapeamento
     
     
     //distancia_atual = distancia do encoder;
     distancia_restante = distancia_total[trecho] - distancia_atual;
-    distancia_necessaria = ((v_final * v_final) - (v_atual * v_atual)) / a;
+    distancia_necessaria = ((v_final * v_final) - (v_atual * v_atual)) / (2*a);
     
     
     if(distancia_necessaria < 0)    distancia_necessaria = 0;   //evita que seja negativo
@@ -37,6 +38,7 @@ void Running_Torricielli(void)
     //robô ainda não chegou no momento de reduzir a velocidade
     {
         //colocamos em velocidade máxima do trecho
+        /*Necessario criar função de conversão de vel. para duty*/
     }
     
     else
