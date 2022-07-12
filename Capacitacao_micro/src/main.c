1ab2ddd154d575386c13adb54e77409a91c0297d
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2022-07-12 16:28:02 -0300

    att

--- Capacitacao_micro/src/main.c
+++ Capacitacao_micro/src/main.c
@@ -1,62 +1,6 @@
-#include <avr/io.h>
-#include <util/delay.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <avr/interrupt.h>
-#include "uart.h"
-#include "ADC.h"
-#include "Display.h"
-#include "PWM.h"
+#include "main.h"
 
-#define set_pin(reg,pin)  (reg |= (1<<pin))
-#define clear_pin(reg,pin)  (reg &= ~(1<<pin))
-#define toggle_pin(reg,pin)  (reg ^= (1<<pin))
-#define test_pin(reg,pin)  (reg & (1<<pin))
-
-/*tempo = 65536 * Prescaler/Fosc = 65536 * 1024/16000000 = 4, 19s   
- * tempo = X_bit_timer * Prescaler/Fosc
- Valor inicial de contagem = 256 - tempo_desejado*Fosc/Prescaler = 256 - 0,01*16000000/1024 = 98,75 = 99
- Valor inicial de contagem = X_bit_timer - tempo_desejado*Fosc/Prescaler*/
-
-//Vi = 256 - (0,001*16000000)/64 = 6 -> 255 = 250 contagens  
-
-void f_timers(void);
-void f_timer1(void);
-void f_timer2(void);
-void ext_int(void);
-void setup(void);
-void loop(void);
-
-volatile char ch;
-volatile char flag_com;
-
-short int Max_timer1 = 0, Max_timer2 = 0;
-unsigned char unidade = 0, dezena = 0, centena = 0;
-
-char buffer [10]  = {0};
-char buffer2 [10] = {0};
-
-ISR(USART_RX_vect)
-{
-  ch  = UDR0;
-  uart_caractere_sending_service(ch);
-  flag_com = 1;
-}
-
-ISR(ADC_vect)
-{
-  tratar_leitura_do_ADC();
-}
-
-ISR (TIMER0_OVF_vect)
-{
-  f_timers();
-}
-
-ISR(PCINT2_vect)
-{
-  //ext_int();
-}
+unsigned int Max_timer1;  //variavel que determina a contagem maxima
 
 int main()
 {
@@ -66,186 +10,40 @@ int main()
   return 0;
 }
 
-void f_timers()
-{
-  static unsigned short int counter1 = 1, counter2 = 1;
-
-  if(counter1 < Max_timer1) counter1++;
-
-  else
-  {
-    //f_timer1(); //acontece a cada 500ms
-    counter1 = 1;
-  }
-
-  if(counter2 < Max_timer2) counter2++;
-
-  else
-  {
-    f_timer2(); //acontece a cada 10ms
-    counter2 = 1;
-  }
-
-}
 
 void setup() 
 {
-  /*DDRD = 0xff;
-  PORTD = 0x00;
-  set_pin(DDRB, PB1);
-  set_pin(DDRB, PB2);
-  set_pin(DDRB, PB3);
-  set_pin(DDRB, PB5);
-  //uart_setup(16); //se colocar display no PD0 comentar esta linha
-  adc_setup();
-  inic_LCD_4bits();
-  escreve_LCD("  Voltimetro");
-  TCCR1A = 0xA3; //Configura operacao em fast PWM, utilizando registradores OCR1x para comparacao
-  setup_pwm_setFreq(11);
-  sei();
-  tratar_leitura_do_ADC();*/
 
-  DDRD = 0x0F;  //0b00001111
+  DDRD = 0x0C;  //0b00001100 -> PD2 e PD3 como saida
   PORTD = 0x00; //0b00000000
-  DDRB = 0x04; //0b00000100
-  PORTB = 0x00;
   TCCR0B = 0x03;  //prescaler de 64
   TCNT0 = 6;      //250 contagens gera 1ms de tempo
   TIMSK0 = 0x01;  //habilito a interrupção do timer0
-  PCICR = 0x04; //0b00000100
-  TCCR1A = 0xA3; //Configura operacao em fast PWM, utilizando registradores OCR1x para comparacao
-  setup_pwm_setFreq(12);
-  PCMSK2 = 0x04;  //habilito a interrupção externa no pino PD2
-  uart_setup(16); //57600 bps
-  adc_setup();
-  inic_LCD_4bits();
-  escreve_LCD("  Voltimetro");
-  tratar_leitura_do_ADC();
-  Max_timer1 = 500;
-  Max_timer2 = 10;  //tempo de 10ms
+  Max_timer1 = 200; //conto até 200 para 200ms de tempo
   sei();  //chave geral das interrupções
 }
 
 void loop() 
 {
-  static float duty = 0;
-  //toggle_pin(PORTD, PD1); //inverto o estado do led
-  //_delay_ms(800);
-  duty = (AD_pins[0]<<2) * 100.0/1023.0;
-  /*static unsigned char unidade, dezena, centena;
-  static int Volt;
-
-  static float duty = 0;
-
-  Volt = (500.0*AD_pins[0])/255.0;
-
-  centena = Volt/100;
-  dezena = (Volt%100)/10;
-  unidade = Volt%10;
-
-  duty = (AD_pins[0]<<2) * 100.0/1023.0;*/
-
-  /*static unsigned char counter = 0;
-  static bool flag_bt = 0;
-  static bool read_bt = 0;
-  static bool option = 0;
-  static char buffer [10] = {0};
-
-  read_bt = test_pin(PINB, PB0);
-
-  if(!read_bt && !flag_bt)
-  {
-    if(!option)
-    {
-      option = 1;
-    }
-    else  option = 0;
-    
-    flag_bt = 1;
-  }
-
-  else if(read_bt && flag_bt)
-  {
-    flag_bt = 0;
-  }
-
-  if(!option)
-  {
-    PORTD = counter;
-    _delay_ms(400);
-  }
-  
-  else 
-  {*/
-    /*if(counter%2)
-    {
-      PORTD = 170;
-    }
-
-    else
-    {
-      PORTD = 85;
-    }*/
-    /*PORTD = AD_pins[0];
-    sprintf(buffer, "%d\n", AD_pins[0]);
-    uart_string_sending_service(buffer);
-    _delay_ms(1);
-  }
-  counter++;*/
-  /*static float Volt = 0;
-
-  Volt = ((5.0*AD_pins[0])/255.0);
-
-  sprintf(buffer, "  %.2f", Volt);
-  cmd_LCD(0xC0, 0); //desloca o cursor para a segunda linha
-  escreve_LCD(buffer);*/
-
-  /*pwm_set_duty_service(AD_pins[0]<<2, PWM_CHANNEL_1);
-  pwm_set_duty_service(AD_pins[0]<<2, PWM_CHANNEL_2);
-  sprintf(buffer, "%d.%d%d duty %3.2f%%", centena, dezena, unidade, duty);
-  cmd_LCD(0xC0, 0); //desloca o cursor para a segunda linha
-  escreve_LCD(buffer);*/
-  sprintf(buffer2, "duty %.2f", duty);
-  cmd_LCD(0xC0, 0); //desloca o cursor para a segunda linha
-  escreve_LCD(buffer2);
+  toggle_bit(PORTD, PD3);
+  _delay_ms(500);
 }
 
-void f_timer1()
+
+void temporizador(void)
 {
-  toggle_pin(PORTD, PD0);
+  static unsigned int contador1 = 1;
+
+  if(contador1 < Max_timer1)  contador1++;
+
+  else
+  {
+    rotina1();
+    contador1 = 1;
+  }
 }
 
-void f_timer2()
+void rotina1(void)
 {
-  static int Volt = 0;
-
-  Volt = (AD_pins[0] * 500.0) /255.0; //255 * 500 /255.0 = 500V
-  centena = Volt/100; //489/100 = 4.89 = 4
-  dezena = (Volt%100)/10; //489%100 = 4.89 -> 89/10 = 8.9 = 8
-  unidade = Volt%10; //489%10 = 48.9 -> 9
-
-  sprintf(buffer, "%d.%d%d\n", centena, dezena, unidade);
-  uart_string_sending_service(buffer);
-
-  pwm_set_duty_service(AD_pins[0]<<2, 2);
-}
-
-void ext_int(void)
-{
-  static bool flag_bt = 0;
-  static unsigned char read_bt = 0;
-
-  read_bt = test_pin(PIND, PD2); //2^n_posicao
-
-  if(!read_bt && flag_bt)
-  {
-    toggle_pin(PORTD, PD3);
-    flag_bt = 0;
-  }
-
-  else if(read_bt && !flag_bt)
-  {
-    flag_bt = 1;
-  }
-
+  toggle_bit(PORTD, PD2);
 }
